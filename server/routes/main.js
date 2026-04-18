acf590a452bab78fa4f8a2b1628ea5ffcd3c9c25
Pedro Lobato <lobato.ph17@gmail.com>
2026-04-17 22:44:24 -0300

    feat: Adiciona navbar, estrutura das pastas, inicio do front-end

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,22 @@ const router = express.Router();
 
 //Routes
 router.get("", (req, res) => {
-  res.send("Hello World");
+  const locals = {
+    title: "Lobato Dev",
+    description: "A blog created with NodeJs, Express & MongoDB.",
+  };
+
+  res.render("index", { locals });
 });
+
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
+router.get("/projects", (req, res) => {
+  res.render("projects");
+});
+
+
 
 module.exports = router;
